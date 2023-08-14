ae451776eba533b30184333221d382e019635db0
fedecassano <fedecassanno@gmail.com>
2023-08-14 15:33:14 -0300

    agrego home

--- views/login.php
+++ views/login.php
@@ -1,7 +1,5 @@
 <link href="../css/bootstrap.css" rel="stylesheet" />
-<!-- Section: Design Block -->
 <section class="">
-  <!-- Jumbotron -->
   <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
     <div class="container">
       <div class="row gx-lg-5 align-items-center">
@@ -22,35 +20,31 @@
           <div class="card">
             <div class="card-body py-5 px-md-5">
               <form>
-                <!-- 2 column grid layout with text inputs for the first and last names -->
                 <div class="row">
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
                       <input type="text" id="form3Example1" class="form-control" />
-                      <label class="form-label" for="form3Example1">First name</label>
+                      <label class="form-label" for="form3Example1">Primer Nombre</label>
                     </div>
                   </div>
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
                       <input type="text" id="form3Example2" class="form-control" />
-                      <label class="form-label" for="form3Example2">Last name</label>
+                      <label class="form-label" for="form3Example2">Apellido</label>
                     </div>
                   </div>
                 </div>
 
-                <!-- Email input -->
                 <div class="form-outline mb-4">
                   <input type="email" id="form3Example3" class="form-control" />
-                  <label class="form-label" for="form3Example3">Email address</label>
+                  <label class="form-label" for="form3Example3">E-mail</label>
                 </div>
 
-                <!-- Password input -->
                 <div class="form-outline mb-4">
                   <input type="password" id="form3Example4" class="form-control" />
                   <label class="form-label" for="form3Example4">Password</label>
                 </div>
 
-                <!-- Checkbox -->
                 <div class="form-check d-flex justify-content-center mb-4">
                   <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
                   <label class="form-check-label" for="form2Example33">
@@ -58,12 +52,10 @@
                   </label>
                 </div>
 
-                <!-- Submit button -->
                 <button type="submit" class="btn btn-primary btn-block mb-4">
                   Sign up
                 </button>
 
-                <!-- Register buttons -->
                 <div class="text-center">
                   <p>or sign up with:</p>
                   <button type="button" class="btn btn-link btn-floating mx-1">
